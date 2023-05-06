a78978973938be5514949ac234363c050eb53fee
Roberth <roberthjot@gmail.com>
2023-05-06 11:10:13 -0400

    node-1

---
+++ app.js
@@ -0,0 +1,24 @@
+require('colors')
+
+const { inquirerMenu } = require('./helpers/inquirer');
+
+
+console.clear()
+
+const main = async() => {
+
+    console.log('hola mundo');
+
+    let opt = '';
+
+    do {
+        opt = await inquirerMenu();
+        console.log( {opt} );
+
+    } while( opt !== '0' )
+
+    // pause();
+
+}
+
+main();
